049dce96871c6e9f7c81317a217913310a32b1d8
Marcus Boykin <awa@Marcuss-MBP.lan>
2023-09-08 23:14:45 -0400

    Added Sum and Average methods

---
+++ src/main/java/AverageGrades.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public class AverageGrades {
+
+
+    static int arr[] = {85, 92, 78,95, 88};
+
+    static int sum() {
+        int sum = 0;
+        int i;
+
+        // Iterate through all elements and add them to sum
+        for (i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+        }
+        return sum;
+    }
+
+    static int average() {
+
+
+        int length = arr.length;
+
+       int sum = sum();
+
+       int average = sum/length;
+
+        return average;
+    }
+
+    public void poop() {
+
+        average();
+
+    }
+
+
+    public static void main (String[] args){
+
+        System.out.println("Sum of given array is: " + sum());
+
+        System.out.println(" The Average of the given array is: " + average());
+
+
+    }
+
+
+
+
+}
+
+/*Student 1:
+Name: John Doe
+Grade: 85
+
+Student 2:
+Name: Jane Smith
+Grade: 92
+
+Student 3:
+Name: Michael Johnson
+Grade: 78
+
+Student 4:
+Name: Emily Davis
+Grade: 95
+
+Student 5:
+Name: David Lee
+Grade: 88
+*/
